c6ad11d943cb5c3c3ebf5040270dd7fe4052d026
shk <2516974158@qq.com>
2023-01-12 14:05:10 +0800

    完成车次增加,修改,删除

--- train-service/src/main/java/com/test/controller/TrainController.java
+++ train-service/src/main/java/com/test/controller/TrainController.java
@@ -1,5 +1,6 @@
 package com.test.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.test.entity.Train;
 import com.test.service.TrainService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,9 +49,59 @@ public class TrainController {
         }
 
         response.put("data", data);
-        System.out.println(response);
+        //System.out.println(response);
         return response;
     }
 
+    @PostMapping("/addTrain")
+    public Map<String, Object> addTrain(@RequestBody String reqBody) {
+        System.out.println(reqBody);
+        JSONObject jsonObject = JSONObject.parseObject(reqBody);
+        Map<String, Object> response = new HashMap<>();
+        String tid = jsonObject.getString("trainId");
+        String stations = jsonObject.getString("stations");
+        int capacity = Integer.parseInt(jsonObject.getString("capacity"));
+        String startTime = jsonObject.getString("startTime").substring(0, 10);
+        String frequency = jsonObject.getString("frequency");
+        Train curTrain = service.getTrainById(tid);
+        if (curTrain == null) {
+            System.out.println(startTime);
+            service.addTrain(tid, stations, capacity, startTime, frequency);
+            response.put("success", true);
+        } else {
+            response.put("success", false);
+        }
+        return response;
+    }
 
+    @GetMapping("/delTrain/{trainId}")
+    public Map<String, Object> delTrain(@PathVariable(name = "trainId") String tid) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            service.deleteTrain(tid);
+            response.put("success", true);
+        } catch (Exception E) {
+            response.put("success", false);
+        }
+        System.out.println("del: " +tid + " , " + response);
+        return response;
+    }
+
+    @PostMapping("modifyTrain/{trainId}")
+    public Map<String, Object> modifyTrain(@PathVariable(name = "trainId") String tid, @RequestBody String reqBody) {
+        Map<String, Object> response = new HashMap<>();
+        JSONObject jsonObject = JSONObject.parseObject(reqBody);
+        Train curTrain = service.getTrainById(tid);
+        int capacity = Integer.parseInt(jsonObject.getString("capacity"));
+        String startTime = jsonObject.getString("startTime");
+        String frequency = jsonObject.getString("frequency");
+        if (curTrain != null) {
+            //
+            service.modifyTrain(tid, curTrain.getCapacity(), startTime, frequency);
+            response.put("success", true);
+        } else {
+            response.put("success", false);
+        }
+        return response;
+    }
 }
